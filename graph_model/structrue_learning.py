39f3d0a239e08e8d348919b48104ca4cff5aa890
wenfeng <zhangwenfeng42153@gmail.com>
2018-05-03 14:10:14 -0500

    code section

--- graph_model/structrue_learning.py
+++ graph_model/structrue_learning.py
@@ -13,10 +13,18 @@ import pandas as pd
 
 #prepare data
 DATA_PATH = parentdir + "\\bpsk_navigate\\data\\test\\"
-
 list_files = get_file_list(DATA_PATH)
-for file in list_files:
-    #TODO
-    #read all data.
-    #store it in pandas and analyze it.
-data = pd.DataFrame()
+graph = 0
+for i, file in enumerate(list_files):
+    data = np.load(DATA_PATH + file)
+    split = int(len(data) / 2)
+    normal = data[:split,:]
+    fault = data[split:,:]
+    df0 = pd.DataFrame(normal)
+    df1 = pd.DataFrame(fault)
+    tmp0 = df0.corr('spearman')
+    tmp1 = df1.corr('spearman')
+    tmp = (tmp0 + tmp1 )/2
+    graph = (graph * (i/(i + 1))) + tmp / (i+1)
+
+print(graph)
