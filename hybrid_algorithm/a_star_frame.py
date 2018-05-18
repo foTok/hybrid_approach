e45b5c9f2fd2b42f69ae6bad3727a111927c4276
Wenfeng <zhangwenfeng42153@gmail.com>
2018-05-18 11:22:17 -0500

    implement a general A* search framework for diagnosis

---
+++ hybrid_algorithm/a_star_frame.py
@@ -0,0 +1,89 @@
+"""
+Defines the framework of A* search algorithm
+"""
+from queue import PriorityQueue
+
+class a_star_frame:
+    """
+    The A* search framework
+    If you want to use it, inherit it and implement self.cost function.
+    Set order and priori firstly and then call search.
+    """
+    def __init__(self):
+        #MEMBER
+        #priority queue to stor candidate
+        self.queue      = PriorityQueue()
+        #order should be a tuple that stores the search order of varibles
+        #for example: (0, 1, 2, 3, 4, 5)
+        #initialized as None
+        self.order      = None
+        #priori probability
+        #priori should be a two-layer tuple that stores the priori probability of varialbe values
+        #for example: ((0.1, 0.9), (0.2, 0.8))
+        #initialized as None
+        self.priori     = None
+        #ACTION
+        #init queue
+        self.queue.put((0,()))
+
+    def cost(self, candidate):
+        """
+        return the cost of the candidate
+        candidate is a tuple where the variables are ordered in self.order
+        and the values are for each corresponding variables.
+        for example ()
+        """
+        pass
+
+
+    def set_order(self, order):
+        """
+        set the variable order
+        """
+        self.order = order
+        
+    def set_priori(self, priori):
+        """
+        set priori probabiltiy
+        """
+        self.priori = priori
+
+    def __expand(self, candidate):
+        """
+        expand the candidate
+        """
+        index = len(candidate)
+        var_id = self.order[index]
+        var_domain_num = len(self.priori[var_id])
+        for i in range(var_domain_num):
+            new_candidate = list(candidate)
+            new_candidate.append(i)
+            new_candidate = tuple(new_candidate)
+            cost = self.cost(new_candidate)
+            self.queue.put((cost, new_candidate))
+
+    def __goal_include(self, candidate):
+        """
+        check if candidate is a goal
+        """
+        assert len(candidate) <= len(self.order)
+        return len(candidate) == len(self.order)
+
+    def search(self, num=1):
+        """
+        step forward
+        num is the num of results to search. 1 by default
+        """
+        assert self.order is not None
+        assert self.priori is not None
+        result = []
+        while len(result) < num:
+        #get best candidate
+            if self.queue.empty():
+                return result
+            best_cost, best_candi = self.queue.get()
+            if self.__goal_include(best_candi):
+                result.append((best_cost, best_candi))
+            else:
+                self.__expand(best_candi)
+        return result
